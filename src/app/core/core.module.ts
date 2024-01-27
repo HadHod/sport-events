c220760251c28924f29d28edc73d7a9815fdc8aa
HadHod <maciej.mlynarski@gmail.com>
2024-01-27 11:43:08 +0100

    page not found small upgrade

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
+
+const COMPONENTS = [PageNotFoundComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CoreRoutingModule],
+  declarations: [COMPONENTS],
+  imports: [CommonModule, CoreRoutingModule, RouterModule],
 })
 export class CoreModule {}
